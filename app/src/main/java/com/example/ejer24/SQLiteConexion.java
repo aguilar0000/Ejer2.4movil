382d528cfdfe41326b846ca45c4b42afbcc2704c
aguilar0000 <aguilaar044@gmail.com>
2024-03-22 22:05:23 -0600

    Initial commit

---
+++ app/src/main/java/com/example/ejer24/SQLiteConexion.java
@@ -0,0 +1,24 @@
+package com.example.ejer24;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.ejer24.transacciones.Transacciones;
+public class SQLiteConexion extends SQLiteOpenHelper{
+    public SQLiteConexion(Context context, String bddname, SQLiteDatabase.CursorFactory factory, int version){
+        super(context, bddname, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(Transacciones.CreateTableFirmas);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(Transacciones.DropTableFirmas);
+        onCreate(db);
+    }
+
+}
+
